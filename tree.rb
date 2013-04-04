251cbd3e0c6e52698dc5049fbb3704784cbab90a
Vladislav Petrov <electronicchest@gmail.com>
2013-04-04 23:50:34 +0400

    Create new function one_or_more

--- tree.rb
+++ tree.rb
@@ -3,8 +3,7 @@ class Print_tree
  def initialize
   clear_screen
 
-  center = ask_user_center_of_tree
-  height = ask_user_height_of_tree
+  center, height = ask_user_center_and_height_of_tree
 
   print_tree_with center, height
  end
@@ -13,21 +12,24 @@ class Print_tree
   print "\e[2J\e[f"
  end
 
- def ask_user_center_of_tree
-  print "Center of the tree(the distance from the left side of the screen): "
-  return gets.to_i
+ def one_or_more(number)
+  return number < 1 ? 1 : number
  end
 
- def ask_user_height_of_tree
+ def ask_user_center_and_height_of_tree
   print "Tree height(always will be multiplied by three): "
-  return gets.to_i
+  height = one_or_more(gets.to_i)
+  print "Center of the tree(the distance from the left side of the screen): "
+  center = one_or_more(gets.to_i)
+  return center, height
  end
 
- def print_tree_with(center, heigth)
-  0.upto(3) { |i| puts ( " " * (center - i) + ("*" * (1+(i * 2))) ) }
+ def print_tree_with(center, height)
+  center += height 
+  0.upto(height) { |i| puts ( " " * (center - i) + ("*" * (1+(i * 2))) ) }
   2.upto(5) { |i| puts ( " " * (center - i) + ("*" * (1+(i * 2))) ) }
   3.upto(7) { |i| puts ( " " * (center - i) + ("*" * (1+(i * 2))) ) }
-  1.upto(3) {     puts ( " " * (center - 1) + ("*" * 3) ) }
+  1.upto(3) { |i| puts ( " " * (center - 1) + ("*" * 3) ) }
  end
 
 end
