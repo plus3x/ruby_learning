174873c1786eeaedddccdfad737d717a324e54c8
Vladislav Petrov <electronicchest@gmail.com>
2013-04-09 03:35:28 +0400

    Ignore swp

--- convertTwoFilesInOne.rb
+++ convertTwoFilesInOne.rb
@@ -7,16 +7,20 @@ class Test_convert_two_files_in_to_one
   else
    return "Class 'Convert_two_files_in_to_one' does't exist!"
   end
+  
+  if (!output_file_exist?)
+   return "Output file does't exist!" 
+  end
 
   if (output_file_is_correct?)
-   return "Test is other, all is right!"
+   return "Test is done!"
   else
-   return "Test is other, output data is not correct"
+   return "Output data is not correct!"
   end
  end
 
  def create_test_file1_and_file2
-
+  
  end
 
  def class_exist?(class_name)
@@ -33,6 +37,5 @@ def clear_screen
 end
 
 clear_screen
-
 puts "Test class 'Convert_two_files_in_to_one'"
 puts Test_convert_two_files_in_to_one.new.test
