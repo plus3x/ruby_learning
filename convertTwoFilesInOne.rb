40b961f053b4cc7c820aeb62e9ee87e45bd20ae6
Vladislav Petrov <electronicchest@gmail.com>
2013-04-09 03:02:05 +0400

    Create function class exist

--- convertTwoFilesInOne.rb
+++ convertTwoFilesInOne.rb
@@ -2,13 +2,13 @@ class Test_convert_two_files_in_to_one
  def test
   file1, file2 = create_test_file1_and_file2
   
-  if (class_exist(Convert_two_files_in_to_one))
+  if (class_exist?("Convert_two_files_in_to_one"))
    @output_file = Convert_two_files_in_one.new(file1, file2)
   else
    return "Class 'Convert_two_files_in_to_one' does't exist!"
   end
 
-  if (output_file_is_correct)
+  if (output_file_is_correct?)
    return "Test is other, all is right!"
   else
    return "Test is other, output data is not correct"
@@ -19,13 +19,20 @@ class Test_convert_two_files_in_to_one
 
  end
 
- def class_exist(this_class)
-
+ def class_exist?(class_name)
+  Object.const_defined?(class_name)
  end
 
- def output_file_is_correct
+ def output_file_is_correct?
 
  end
 end
 
-Test_convert_two_files_in_to_one.new.test
+def clear_screen
+ print "\e[2J\e[f"
+end
+
+clear_screen
+
+puts "Test class 'Convert_two_files_in_to_one'"
+puts Test_convert_two_files_in_to_one.new.test
