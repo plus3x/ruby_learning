a31ee04cfa80b70ff72933553362c564bf1d8f9a
Vladislav Petrov <electronicchest@gmail.com>
2013-04-12 17:40:55 +0400

    Trying recurtion

--- gameWords.rb
+++ gameWords.rb
@@ -2,32 +2,36 @@ class TestGameWords
   def output_correct?
     input_chain = %w(Bob Mark Karl Lara Yan Buk Kim)
     true_chain = %w(Bob Buk Kim Mark Karl Lara)
-    true_chain == GameWords.new.game(input_chain)
+    true_chain == GameWords.new(input_chain)
   end
 end
 
 class GameWords
   def initialize(chain)
     return %(Chain of words is empty!) if chain == nil
-    return %(Chain of words have bad word!) if chain.each { |word| return false unless word[/[a-z]{2,}/i] }
+    return %(Chain of words have bad word!) unless chain_is_good? chain
   
-    puts "Data: #{chain}"
-    chain.each { |word| puts "#{word} = #{word[0].downcase} #{word[/.$/].downcase}" }
-    puts "Chains get: #{@@chains = chains_get chain}"
+    puts "Chains get: #{chains_get chain}"
+  end
+
+  def chain_is_good?(chain)
+    chain.each { |word| return false unless word[/[a-z]{2,}/i] } != nil
   end
 
   def chains_get(chain)
-    chains = ""
-    (chain - [chain[0]]).each { |word|
-        if chain[0][/.$/].downcase == word[0].downcase
-           puts %(#{chain[0][/.$/].downcase} #{word[0].downcase})
-	   chains << word << chains_get([word] + (chain - [word] - chain[0]))
-	end
+    chains = ''
+    chain.each { |word_| 
+      (chain - [chain[0]]).each { |word| puts %(#{chain[0]} #{word} #{chain[0][/.$/].downcase} #{word[0].downcase})
+        (chains << 
+	chain[0] << 
+	chains_get([word] + (chain - [word] - [chain[0]])) << 
+	word) if 
+	chain[0][/.$/].downcase == word[0].downcase
       }
-    chains + " "
+    }
   end
 end
 
-puts %(Output chain is: #{TestGameWord.new.output_corrrect?})
+#puts %(Test: output chain is #{TestGameWords.new.output_correct? ? 'good' : 'bad'}.)
 puts %(Call class GameWords)
 puts GameWords.new(%w(bm Mark BoB Jamy))
